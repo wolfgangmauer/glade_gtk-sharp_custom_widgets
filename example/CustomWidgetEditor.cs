9c38f991b60fac2a58103a39d406e932c8ed41c1
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2018-09-27 17:07:32 +0200

    Add files via upload

---
+++ example/CustomWidgetEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+
+namespace design_namespace
+{
+	public class custom-widget-name + "Editor" : Box
+    {
+		// .ctor must look like this, parameter is the parent GtkBox
+		public custom-widget-name + "Editor"(IntPtr rawParent)
+			: base(Orientation.Vertical, 0)
+		{
+			try
+			{
+				// parent is a box !!!
+				var parent = new Box(rawParent);
+				parent.PackStart(this, false, false, 0);
+
+				// add whatever your editor needed
+				......
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+    }
+}
