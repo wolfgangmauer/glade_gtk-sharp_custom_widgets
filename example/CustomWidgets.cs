55083428aa4a010649f666c3a094d47d0f1858c0
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2018-09-27 17:11:58 +0200

    Update CustomWidgets.cs

--- example/CustomWidgets.cs
+++ example/CustomWidgets.cs
@@ -4,52 +4,52 @@ using Gtk;
 namespace runtime_namespace
 {
 	public static class GTypeRegister // .config -> gtyperegister_static_class=
-    {
-        internal static void Register() // .config -> gtyperegister_static_function=
-        {
+	{
+        	internal static void Register() // .config -> gtyperegister_static_function=
+        	{
 			ExtTextBox.Register();
 			ExtComboBox.Register();
 			......
-        }
-    }
+        	}
+    	}
 
 	public class ExtTextBox : ....... 
-    {
+    	{
 		public ExtTextBox(IntPtr raw) 
 			: base(raw)
-        {	
+        	{	
 		}
 
 		public ExtTextBox() 
 			: base(IntPtr.Zero)
-        {	
+        	{	
 		}
 
 		// remember, in gtk-sharp the gtypes are look like this 
 		// __gtksharp_X_runtime_namespace_ExtTextBox
 		public static void Register()
-        {
+        	{
 			Debug.WriteLine($"Registering GType {RegisterGType(typeof(ExtTextBox))}");
-        }
-    }
+        	}
+    	}
 
 	public class ExtComboBox : ....... 
-    {
+    	{
 		public ExtComboBox(IntPtr raw) 
 			: base(raw)
-        {	
+        	{	
 		}
 
 		public ExtComboBox() 
 			: base(IntPtr.Zero)
-        {	
+        	{	
 		}
 
 		// remember, in gtk-sharp the gtypes are look like this 
 		// __gtksharp_X_runtime_namespace_ExtTextBox
 		public static void Register()
-        {
+        	{
 			Debug.WriteLine($"Registering GType {RegisterGType(typeof(ExtComboBox))}");
-        }
-    }
+        	}
+    	}
 }
